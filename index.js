22188def06421a5f6896b65ae0989ffc31a3c74f
Samantha VonCannon <svoncannon23@gmail.com>
2023-01-03 15:42:43 -0500

    finished task 1

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ Use the copy function below to do the following:
 */
 
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  return[...array]
 }
 
-
+console.log(copy(originalFlavors))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Confirm that an array is exactly 31 flavors. Your function should accept:
